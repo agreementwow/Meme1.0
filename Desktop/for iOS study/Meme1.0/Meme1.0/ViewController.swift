66b49655f66705f986d9511348bad68273569fe5
agreementwow <agreementwow@gmail.com>
2016-08-30 08:47:11 +0800

    add image feature

---
+++ Desktop/for iOS study/Meme1.0/Meme1.0/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Meme1.0
+//
+//  Created by liulei on 16/8/29.
+//  Copyright © 2016年 liulei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
+        super.viewWillAppear(animated)
+    }
+
+
+    @IBAction func pickImageFromAlbum(sender: AnyObject) {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = .PhotoLibrary
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickImageFromCamera(sender: AnyObject) {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = .Camera
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func cancelActions(sender: AnyObject) {
+        imagePickerView.image = nil
+    }
+}
+
